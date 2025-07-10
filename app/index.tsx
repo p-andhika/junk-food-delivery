090e45bab6180f3233f36d2c7814697e60089c7e
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-07-11 06:36:38 +0700

    style offer card layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,50 @@
-import { Text, View } from "react-native";
+import { offers } from "@/constans";
+import cn from "clsx";
+import { Fragment } from "react";
+import { FlatList, Pressable, View, Text, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl text-center font-bold font-quicksand-bold text-primary">
-        Welcome to my React Native App!
-      </Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row",
+                )}
+                style={{
+                  backgroundColor: item.color,
+                }}
+              >
+                {({ pressed }) => {
+                  return (
+                    <Fragment>
+                      <View className="h-full w-1/2">
+                        <Image
+                          source={item.image}
+                          className="size-full"
+                          resizeMode="contain"
+                        />
+                      </View>
+
+                      <View className="offer-card__info">
+                        <Text>{item.title}</Text>
+                      </View>
+                    </Fragment>
+                  );
+                }}
+              </Pressable>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
