a9a0d3d793686c75586af9420004c0a5c9b2cf98
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-07-12 06:41:53 +0700

    refactor(auth): update sign-in UI and navigation logic

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,11 @@
+import CustomButton from "@/components/custom-button";
 import CustomInput from "@/components/custom-input";
-import { router } from "expo-router";
-import { View, Text, Button } from "react-native";
+import { Link } from "expo-router";
+import { View, Text } from "react-native";
 
 const SignIn = () => {
   return (
     <View className="gap-10 bg-white rounded-lg p-5 mt-5">
-      <Text>Sign In</Text>
-      <Button title="Sign Up" onPress={() => router.push("/sign-up")} />
-
       <CustomInput
         placeholder="Enter your email"
         value=""
@@ -15,6 +13,25 @@ const SignIn = () => {
         label="Email"
         keyboardType="email-address"
       />
+
+      <CustomInput
+        placeholder="Enter your password"
+        value=""
+        onChangeText={(text) => {}}
+        label="Password"
+        secureTextEntry
+      />
+
+      <CustomButton title="Sign In" />
+
+      <View className="flex justify-center mt-5 flex-row gap-2">
+        <Text className="base-regular text-gray-100">
+          Don&lsquo;t have account?
+        </Text>
+        <Link href="/sign-up" className="base-bold text-primary">
+          Sign Up
+        </Link>
+      </View>
     </View>
   );
 };
