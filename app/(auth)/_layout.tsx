b0bc432e54512cb4d680147f1a08807f7df02c0e
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-07-11 09:54:03 +0700

    feat: add auth and tabs layouts with navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,9 +1,12 @@
+import { Slot } from "expo-router";
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AuthLayout() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Auth Layout</Text>
-    </View>
+      <Slot />
+    </SafeAreaView>
   );
 }
