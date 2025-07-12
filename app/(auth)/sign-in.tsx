7847941de3e8d6896d291022f823cd6183eeac83
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-07-12 19:51:44 +0700

    feat(auth): add form state and submission logic

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,28 +1,54 @@
 import CustomButton from "@/components/custom-button";
 import CustomInput from "@/components/custom-input";
-import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { Link, router } from "expo-router";
+import { useState } from "react";
+import { View, Text, Alert } from "react-native";
 
 const SignIn = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "Please enter valid email address & password.");
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      // call appwrite signin function
+
+      Alert.alert("Success", "User signed in successfully.");
+
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <View className="gap-10 bg-white rounded-lg p-5 mt-5">
       <CustomInput
         placeholder="Enter your email"
-        value=""
-        onChangeText={(text) => {}}
+        value={form.email}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
         label="Email"
         keyboardType="email-address"
       />
 
       <CustomInput
         placeholder="Enter your password"
-        value=""
-        onChangeText={(text) => {}}
+        value={form.password}
+        onChangeText={(text) =>
+          setForm((prev) => ({ ...prev, password: text }))
+        }
         label="Password"
         secureTextEntry
       />
 
-      <CustomButton title="Sign In" />
+      <CustomButton title="Sign In" isLoading={isSubmitting} onPress={submit} />
 
       <View className="flex justify-center mt-5 flex-row gap-2">
         <Text className="base-regular text-gray-100">
