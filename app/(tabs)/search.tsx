8a8ccf550f4fea26d764cb1616c9b4addc775a6c
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-07-13 07:06:33 +0700

    feat(search): enhance UI with FlatList and header components

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,8 +1,11 @@
+import cn from "clsx";
+import CartButton from "@/components/cart-button";
 import { getCategories, getMenu } from "@/lib/appwrite";
 import useAppwrite from "@/lib/useAppwrite";
+import clsx from "clsx";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect } from "react";
-import { Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
@@ -29,8 +32,53 @@ const Search = () => {
   }, [category, query]);
 
   return (
-    <SafeAreaView>
-      <Text>Search</Text>
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => {
+          const isFirstRightColItem = index % 2 === 0;
+
+          return (
+            <View
+              className={cn(
+                "flex-1 max-w-[48%]",
+                isFirstRightColItem ? "mt-10" : "mt-0",
+              )}
+            >
+              <Text>Menu Card</Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.$id}
+        numColumns={2}
+        columnWrapperClassName="gap-7"
+        contentContainerClassName="gap-7 px-5 pb-32"
+        ListHeaderComponent={() => {
+          return (
+            <View className="my-5 gap-5">
+              <View className="flex-between flex-row w-full">
+                <View className="flex-start">
+                  <Text className="small-bold uppercase text-primary">
+                    Search
+                  </Text>
+                  <View className="flex-start flex-row gap-x-1 mt-0.5">
+                    <Text className="paragraph-semibold text-dark-100">
+                      Find your favorite food
+                    </Text>
+                  </View>
+                </View>
+
+                <CartButton />
+              </View>
+
+              <Text>Search Input</Text>
+
+              <Text>Filter</Text>
+            </View>
+          );
+        }}
+        ListEmptyComponent={() => !loading && <Text>No Result</Text>}
+      />
     </SafeAreaView>
   );
 };
